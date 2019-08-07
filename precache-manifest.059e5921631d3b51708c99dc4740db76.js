4019acda9b678eb3a782b41cf23fc93693e1cdd6
Danilo Burgos <ilianburgos@gmail.com>
2019-08-06 23:11:50 -0500

    first commit

---
+++ precache-manifest.059e5921631d3b51708c99dc4740db76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83020a8e13d26dde8261544223755236",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3049d8e3411b9593af2",
+    "url": "/static/css/main.e6356d01.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e3049d8e3411b9593af2",
+    "url": "/static/js/main.9202d99d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
